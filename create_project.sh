d9ed35846e4400e788873606926b05b6463916da
Cayo Eduardo <cayo.eduardo.silveira@gmail.com>
2020-11-01 23:21:31 -0300

    Adicionado: react-redux, actions e reducers na pasta redux, services para api e utils para scripts auxiliares

--- create_project.sh
+++ create_project.sh
@@ -2,14 +2,22 @@
 
 npx create-react-app $1
 npm install redux
+npm install react-redux
 npm install redux-thunk
 npm install @material-ui/core
 npm install @material-ui/icons
 npm install react-router-dom
+npm install axios 
+
 cd $(pwd)/$1
 mkdir $(pwd)/components
+mkdir $(pwd)/services
+mkdir $(pwd)/utils
 mkdir $(pwd)/assets
 mkdir $(pwd)/navigation
-mkdir $(pwd)/redux
 mkdir $(pwd)/pages
+mkdir $(pwd)/redux
+cd $(pwd)/redux
+mkdir /actions
+mkdir /reducers
 
