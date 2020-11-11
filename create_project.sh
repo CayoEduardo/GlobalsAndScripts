4922a2899893fc0fd9587d051c219f965b3578d3
Cayo Eduardo <cayo.eduardo.silveira@gmail.com>
2020-11-11 19:02:26 -0300

    Fixed redux subfolders

--- create_project.sh
+++ create_project.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
 npx create-react-app $1
+echo 'create-react-app finished'
 npm install redux
+echo 'redux instaled'
 npm install react-redux
+echo 'react-redux instaled'
 npm install redux-thunk
+echo 'redux-tunk instaled'
 npm install @material-ui/core
+echo 'mui core instaled'
 npm install @material-ui/icons
+echo 'mui icons instaled'
 npm install react-router-dom
+echo 'react-router-dom instaled'
 npm install axios 
+echo 'axios instaled'
 npm install --save-dev nodemon
+echo 'nodemon instaled'
+npm install redux-devtools-extension
+echo 'redux-devtools-extension instaled'
+npm install redux-logger
+echo 'redux- installed'
 
 cd $(pwd)/$1
 touch Dockerfile
@@ -21,6 +34,7 @@ mkdir $(pwd)/navigation
 mkdir $(pwd)/pages
 mkdir $(pwd)/redux
 cd $(pwd)/redux
-mkdir /actions
-mkdir /reducers
+mkdir $(pwd)/actions
+mkdir $(pwd)/reducers
+mkdir $(pwd)/types
 
